664c86ec7e94dfe7947562369b2601314b8ff52f
xames3 <xames3.developer@gmail.com>
2021-03-07 21:52:01 +0530

    Remove support for pyproject.toml and update setup
    Removed support for `pyproject.toml` due to hacky support and confusing
    implmentation. Instead adopted installation over `setup.py` and
    the declarative metadata type using`setup.cfg`.
    
    In `setup.py` added few more entries to the classifiers and did minor
    refactoring, whereas in the `setup.cfg` added support for sphinx, mypy,
    flake8 and check-manifest.

--- setup.py
+++ setup.py
@@ -11,47 +11,53 @@ from setuptools import find_packages, setup
 classifiers = [
     "Development Status :: 4 - Beta",
     "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX",
+    "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Topic :: Software Development :: Libraries",
     "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Utilities",
 ]
 
-dependencies = []
+dev_dependencies = [
+    "black",
+    "flake8",
+    "mypy",
+]
+
+test_dependencies = [
+    "pytest",
+    "tox",
+]
 
 setup(
     name="miroslava",
-    version="1.0.0.dev1",
+    version="1.0.0",
+    author="XAMES3",
+    author_email="xames3.developer@gmail.com",
+    url="https://github.com/kaamiki/miroslava",
     description="A simple sandbox environment",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/kaamiki/miroslava",
-    author="XAMES3",
-    author_email="xames3.developer@gmail.com",
-    license="MIT",
     classifiers=classifiers,
     keywords="python, miroslava, kaamiki",
+    license="MIT",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    python_requires=">=3.6,<4",
-    install_requires=dependencies,
+    python_requires=">=3.6",
     extras_require={
-        "dev": [
-            "black",
-            "check-manifest",
-            "flake8",
-            "pytest",
-            "pytest-cov",
-            "tox",
-            "twine",
-        ],
-        "test": ["coverage", "pytest", "pytest-cov", "tox"],
+        "dev": dev_dependencies,
+        "test": test_dependencies,
     },
     project_urls={
         "Source": "https://github.com/kaamiki/miroslava",
-        "Bug Reports": "https://github.com/kaamiki/miroslava/issues",
+        "Tracker": "https://github.com/kaamiki/miroslava/issues",
     },
+    zip_safe=False,
 )
